777feb5ac498553d488b4004700e9a565e58c3ae
alexegan <42589526+alexegan@users.noreply.github.com>
2019-10-02 21:02:17 -0600

    Added movement and collision

---
+++ Assets/_Scripts/_Scripts_AlexsMoveScripts/_script_Movement.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _script_Movement : MonoBehaviour
+{
+
+    //private bool shield;
+    public float MovementSpeed = 0.5f;
+    public float DiagonalMoveSpeedMultiplier = 1f;
+    private float horizontal, vertical;
+    Rigidbody2D Body;
+    //public GameObject PlayerShield;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Body = GetComponent<Rigidbody2D>();
+        /*if (PlayerShield == null)
+        {
+            Debug.Log("error - couldn't find object for playershield");
+        }
+        */
+        Body.freezeRotation = true;
+        //shield = false;
+    }
+
+
+    private void MovePlayer()
+    {
+        
+        Vector2 pos = Body.velocity;
+        if (vertical != 0)
+        {
+            pos.y = MovementSpeed * Mathf.Sign(vertical);
+        }
+        else
+        {
+            pos.y = 0;
+        }
+        if (horizontal != 0)
+        {
+            pos.x = MovementSpeed * Mathf.Sign(horizontal);
+        }
+        else
+        {
+            pos.x = 0;
+        }
+        if (vertical != 0 && horizontal != 0)
+        {
+            float average = MovementSpeed / 2;
+            pos.x = average * Mathf.Sign(horizontal);
+            pos.y = average * Mathf.Sign(vertical);
+        }
+
+        Body.velocity = pos;
+        return;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+        //Abilities();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        /*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shield = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            shield = false;
+        }*/
+    }
+
+    //private GameObject shieldup;
+    /*
+    private void Abilities()
+    {
+        if (shield)
+        {
+            if (shieldup == null)
+            {
+                shieldup = Instantiate(PlayerShield, transform);
+            }
+            else
+            {
+                shieldup.transform.position = transform.position;
+            }
+        }
+        else
+        {
+            if (shieldup != null)
+            {
+                Destroy(shieldup);
+                shieldup = null;
+            }
+        }
+    }
+    */
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+
+    }
+
+}
---
+++ Assets/_Scripts/_Scripts_AlexsMoveScripts/_script_PushBlockLogic.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class _script_PushBlockLogic : MonoBehaviour
+{
+    Rigidbody2D body;
+    public bool left = false;
+    public bool right = false;
+    public bool up = false;
+    public bool down = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        int hitpoints = collision.contactCount;
+        // Debug.Log("hitpoints: " + hitpoints);
+        ContactPoint2D hit = collision.GetContact(hitpoints - 1);
+        //Debug.Log("--- HIT: " + hit.collider.name);
+        if (!hit.Equals(null))
+        {
+            //if (hit.collider.name == "Player")
+            {
+                float collide_y = hit.collider.transform.position.y;
+                float collide_x = hit.collider.transform.position.x;
+                float contact_y_diff = (collide_y - this.transform.position.y);
+                float contact_x_diff = (collide_x - this.transform.position.x);
+
+
+                Debug.Log("Contact successful -- Contact point: " + collide_x + " - " + this.name + " position: " + this.transform.position.x);
+                if (up && contact_y_diff < 0 && System.Math.Abs(contact_y_diff) > 0.21 && System.Math.Abs(contact_y_diff) < 0.24)
+                {
+                    Debug.Log("-----------Contact from below");
+                    body.constraints = RigidbodyConstraints2D.None;
+                    body.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+                }
+                else if (down && contact_y_diff > 0 && System.Math.Abs(contact_y_diff) > 0.71 && System.Math.Abs(contact_y_diff) < 0.74)
+                {
+                    Debug.Log("-----------Contact from above");
+                    body.constraints = RigidbodyConstraints2D.None;
+                    body.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+                }
+                else if (left && contact_x_diff > 0 && System.Math.Abs(contact_x_diff) > 0.70 && System.Math.Abs(contact_x_diff) < 0.74)
+                {
+                    Debug.Log("-----------Contact from the right");
+                    body.constraints = RigidbodyConstraints2D.None;
+                    body.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+                }
+                else if (right && contact_x_diff < 0 && System.Math.Abs(contact_x_diff) > 0.20 && System.Math.Abs(contact_x_diff) < 0.24)
+                {
+                    Debug.Log("-----------Contact from the left");
+                    body.constraints = RigidbodyConstraints2D.None;
+                    body.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+                }
+                else
+                {
+                    body.constraints = RigidbodyConstraints2D.FreezeAll;
+                }
+            }
+        }
+    }
+
+}
