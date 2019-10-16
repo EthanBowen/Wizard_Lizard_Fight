c824a029f729563a31528adea6a58b234d99cece
alexegan <42589526+alexegan@users.noreply.github.com>
2019-10-16 16:57:27 -0600

    Game end screen works kinda

---
+++ Assets/_Scripts/_script_GameEndCanvasScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class _script_GameEndCanvasScript : MonoBehaviour
+{
+    int winner;
+    public Text instruction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winner = 0;
+        winner = PlayerPrefs.GetInt("winner");
+
+        instruction = GetComponent<Text>();
+
+        instruction.text = "WINNER: " + winner + "\n Press space to restart";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
