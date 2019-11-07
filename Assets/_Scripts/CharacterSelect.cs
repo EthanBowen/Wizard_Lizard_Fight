c29b0e4e7ab77b148e2866206e674c6497e3fafc
IamTheStitch <46461384+IamTheStitch@users.noreply.github.com>
2019-11-06 21:00:01 -0700

    Made multiple player UI

--- Assets/_Scripts/CharacterSelect.cs
+++ Assets/_Scripts/CharacterSelect.cs
@@ -6,20 +6,39 @@ using UnityEngine.UI;
 
 public class CharacterSelect : MonoBehaviour
 {
-    public int ID;
-    private PlayerUI player;
-    private int playerCharacter;
+    //player ID
+    public int ID = 1;
+    //Player panels
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
+    public GameObject Player4;
 
+    private int playerCharacter;
+    //to track the players choice for character
     private int selectedCharacterIndex;
+    //color of the character
     private Color desiredColor;
 
     [Header("List of Characters")]
     [SerializeField] public List<CharacterSelectObject> characterList = new List<CharacterSelectObject>();
 
     [Header("UI Reference")]
-    [SerializeField] private TextMeshProUGUI characterName;
-    [SerializeField] private Image characterSplash;
-    [SerializeField] private Image characterColor;
+
+    [SerializeField] private TextMeshProUGUI player1Name;
+    [SerializeField] private TextMeshProUGUI player2Name;
+    [SerializeField] private TextMeshProUGUI player3Name;
+    [SerializeField] private TextMeshProUGUI player4Name;
+
+    [SerializeField] private Image player1Splash;
+    [SerializeField] private Image player2Splash;
+    [SerializeField] private Image player3Splash;
+    [SerializeField] private Image player4Splash;
+
+    [SerializeField] private Image player1Color;
+    [SerializeField] private Image player2Color;
+    [SerializeField] private Image player3Color;
+    [SerializeField] private Image player4Color;
     
     [Header("Sounds")]
     [SerializeField] private AudioClip arrowSFX;
@@ -37,16 +56,47 @@ public class CharacterSelect : MonoBehaviour
     private void Update()
     {
         //slowly changes the background color to the designated color of the player character background
-        characterColor.color = Color.Lerp(characterColor.color, desiredColor, Time.deltaTime * backgroundColorTransitionSpeed);
+        player1Color.color = Color.Lerp(player1Color.color, desiredColor, Time.deltaTime * backgroundColorTransitionSpeed);
+        player2Color.color = Color.Lerp(player2Color.color, desiredColor, Time.deltaTime * backgroundColorTransitionSpeed);
+        player3Color.color = Color.Lerp(player3Color.color, desiredColor, Time.deltaTime * backgroundColorTransitionSpeed);
+        player4Color.color = Color.Lerp(player4Color.color, desiredColor, Time.deltaTime * backgroundColorTransitionSpeed);
+        countPlayers();
+        switch(ID)
+        {
+            case (1):
+                Player2.SetActive(false);
+                Player3.SetActive(false);
+                Player4.SetActive(false);
+                break;
+            case (2):
+                Player2.SetActive(true);
+                Player3.SetActive(false);
+                Player4.SetActive(false);
+                break;
+            case (3):
+                Player2.SetActive(true);
+                Player3.SetActive(true);
+                Player4.SetActive(false);
+                break;
+            case (4):
+                Player2.SetActive(true);
+                Player3.SetActive(true);
+                Player4.SetActive(true);
+                break;
+        }
+    }
+    private void countPlayers()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter) && ID < 4)
+             ID++;
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Delete) && ID > 1)
+             ID--;
     }
     //Meant to store the information on multiple players
     public void playerTracker()
     {
         switch(ID)
         {
-            case (0):
-                playerCharacter = selectedCharacterIndex;
-                break;
             case (1):
                 playerCharacter = selectedCharacterIndex;
                 break;
@@ -54,6 +104,9 @@ public class CharacterSelect : MonoBehaviour
                 playerCharacter = selectedCharacterIndex;
                 break;
             case (3):
+                playerCharacter = selectedCharacterIndex;
+                break;
+            case (4):
                 playerCharacter = selectedCharacterIndex;
                 break;
         }
@@ -89,16 +142,16 @@ public class CharacterSelect : MonoBehaviour
         //TODO: make this function save the information the player chooses
         switch(ID)
         {
-            case (0):
+            case (1):
                 PlayerPrefs.SetInt("Player1", selectedCharacterIndex);
                 break;
-            case (1):
+            case (2):
                 PlayerPrefs.SetInt("Player2", selectedCharacterIndex);
                 break;
-            case (2):
+            case (3):
                 PlayerPrefs.SetInt("Player3", selectedCharacterIndex);
                 break;
-            case (3):
+            case (4):
                 PlayerPrefs.SetInt("Player4", selectedCharacterIndex);
                 break;
         }
@@ -110,8 +163,14 @@ public class CharacterSelect : MonoBehaviour
     private void updateCharacterSelectUI()
     {
         //Sets the Splash, Name, Color
-        characterSplash.sprite = characterList[selectedCharacterIndex].splash;
-        characterName.text = characterList[selectedCharacterIndex].characterName;
+        player1Splash.sprite = characterList[selectedCharacterIndex].splash;
+        player1Name.text = characterList[selectedCharacterIndex].characterName;
+        player2Splash.sprite = characterList[selectedCharacterIndex].splash;
+        player2Name.text = characterList[selectedCharacterIndex].characterName;
+        player3Splash.sprite = characterList[selectedCharacterIndex].splash;
+        player3Name.text = characterList[selectedCharacterIndex].characterName;
+        player4Splash.sprite = characterList[selectedCharacterIndex].splash;
+        player4Name.text = characterList[selectedCharacterIndex].characterName;
         desiredColor = characterList[selectedCharacterIndex].characterColor;
     }
 
--- Assets/_Scripts/PlayerUI.cs
+++ Assets/_Scripts/PlayerUI.cs
@@ -11,47 +11,37 @@ public class PlayerUI : MonoBehaviour
     public Player player;
     public HealthBar HP;
     public ManaBar MP;
+    public GameObject playerPicture;
     public float health = 0;
     public float mana = 0;
     public Sprite playerSprite;
     public Sprite[] characterList;
-    //GameObject healthbar = Instantiate(Resources.Load("HealthBarPrefab"), new Vector3(0.118f, .960f, 5f), Quaternion.identity) as GameObject;
-   // GameObject manabar = Instantiate(Resources.Load("ManaBarPrefab"), new Vector3(0.118f, 0.943f, 5f), Quaternion.identity) as GameObject;
-
-    //public Sprite playerSprite2;
-
-    private SpriteRenderer sr;
     // Start is called before the first frame update
     void Start()
     {
        UI_Camera = GameObject.FindGameObjectWithTag("UI Camera").GetComponent<Camera>();
-       //WIP Character models next to UI
-       //sr = gameObject.AddComponent<SpriteRenderer>();
-       //playerSprite = characterList[0];
-       //if (sr.sprite == null)
-        //   sr.sprite = playerSprite;
-       //brings the picture of the sprite in front of the HP and MP bars
-        
+               
        //sets the locations of all players spawned in their respected corners of the screen
        switch (ID)
        {
             case 1: //top left player
-                x = 0.118f;
+                x = 0.04f;
                 y = .940f;
                 this.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(x, y, 10f));
                 break;
             case 2: //top right player
-                x = 1 - 0.118f;
+                x = 1 - 0.15f;
                 y = 0.940f;
                 this.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(x, y, 10f));
+
                 break;
             case 3: //bottom left player
-                x = 0.118f;
+                x = 0.04f;
                 y = 1 - 0.940f;
                 this.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(x, y, 10f));
                 break;
             case 4: //bottom right player
-                x = 1-0.118f;
+                x = 1 - 0.15f;
                 y = 1 - 0.940f;
                 this.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(x, y, 10f));
                 break;
@@ -64,10 +54,6 @@ public class PlayerUI : MonoBehaviour
     {
         health = player.health;
         mana = player.MP;
-        //WIP to figure out with Character Selection for later
-        //if (Input.GetKeyDown(KeyCode.Space))
-        // ChangePlayerSprite();
-
     }
 
     private void ChangePlayerSprite()
@@ -79,21 +65,25 @@ public class PlayerUI : MonoBehaviour
     {
         switch(ID)
         {
-            case (1):
-                this.gameObject.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player1")];
-                this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 100;
+            case (1): //top left player
+                playerPicture.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player1")];
+                playerPicture.GetComponent<SpriteRenderer>().sortingOrder = 100;
                 break;
-            case (2):
-                this.gameObject.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player2")];
-                this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 100;
+            case (2): //top right player
+                playerPicture.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player2")];
+                playerPicture.GetComponent<SpriteRenderer>().sortingOrder = 100;
+                playerPicture.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.95f, 0.940f, 10f));
+                playerPicture.GetComponent<SpriteRenderer>().flipX = true;
                 break;
-            case (3):
-                this.gameObject.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player3")];
-                this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 100;
+            case (3): //bottom left player
+                playerPicture.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player3")];
+                playerPicture.GetComponent<SpriteRenderer>().sortingOrder = 100;
                 break;
-            case (4):
-                this.gameObject.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player4")];
-                this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 100;
+            case (4): //bottom right player
+                playerPicture.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player4")];
+                playerPicture.GetComponent<SpriteRenderer>().sortingOrder = 100;
+                playerPicture.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.95f, 1 - 0.940f, 10f));
+                playerPicture.GetComponent<SpriteRenderer>().flipX = true;
                 break;
         }
         
