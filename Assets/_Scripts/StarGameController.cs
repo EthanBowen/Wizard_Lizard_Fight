a04a6da29c3615d45d67c1b8b2ec9e54c60233ff
IamTheStitch <46461384+IamTheStitch@users.noreply.github.com>
2019-11-09 16:15:37 -0700

    Created Start Gfame Scene and  added the buitton

---
+++ Assets/_Scripts/StarGameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class StarGameController : MonoBehaviour
+{
+    [Header("Game Sounds")]
+    public AudioSource titleSong;
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleSong.loop = true;
+        titleSong.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game_MJ");
+    }
+}
