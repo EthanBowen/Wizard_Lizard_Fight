de41551646a52326fc27ca7df82d3cb63cd1791c
IamTheStitch <46461384+IamTheStitch@users.noreply.github.com>
2019-11-13 01:10:36 -0700

    Changed to where the button went

--- Assets/_Scripts/CharacterSelect.cs
+++ Assets/_Scripts/CharacterSelect.cs
@@ -443,21 +443,21 @@ public class CharacterSelect : MonoBehaviour
             case (2):
                 if (player1Ready && player2Ready)
                 {
-                    SceneManager.LoadScene("Game_MJ_Test");
+                    SceneManager.LoadScene("Game");
                     characterSelectMusic.Stop();
                 } 
                 break;
             case (3):
                 if (player1Ready && player2Ready && player3Ready)
                 {
-                    SceneManager.LoadScene("Game_MJ_Test");
+                    SceneManager.LoadScene("Game");
                     characterSelectMusic.Stop();
                 }
                 break;
             case (4):
                 if (player1Ready && player2Ready && player3Ready && player4Ready)
                 {
-                    SceneManager.LoadScene("Game_MJ_Test");
+                    SceneManager.LoadScene("Game");
                     characterSelectMusic.Stop();
                 }
                 break;
