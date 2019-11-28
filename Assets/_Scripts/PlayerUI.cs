e2daa6789cb5ec4c1b3f81db944078613cb67db6
Ethan Bowen <Ethankbowen@gmail.com>
2019-11-27 21:09:27 -0700

    UI has been moved around to be more even

--- Assets/_Scripts/PlayerUI.cs
+++ Assets/_Scripts/PlayerUI.cs
@@ -32,7 +32,7 @@ public class PlayerUI : MonoBehaviour
                 this.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(x, y, 10f));
                 break;
             case 2: //top right player
-                x = 1 - 0.15f;
+                x = 1 - 0.143f;
                 y = 0.900f;
                 this.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(x, y, 10f));
 
@@ -43,7 +43,7 @@ public class PlayerUI : MonoBehaviour
                 this.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(x, y, 10f));
                 break;
             case 4: //bottom right player
-                x = 1 - 0.15f;
+                x = 1 - 0.143f;
                 y = 1 - 0.900f;
                 this.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(x, y, 10f));
                 break;
@@ -71,37 +71,37 @@ public class PlayerUI : MonoBehaviour
         {
             case (1): //top left player
                 playerPicture.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player1")];
-                playerPicture.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.04f, 0.900f, 10f));
+                playerPicture.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.035f, 0.860f, 10f));
                 scoreText.transform.localPosition = new Vector3(-780f, 401f, 10f);
                 playerPicture.GetComponent<SpriteRenderer>().sortingOrder = 100;
                 playerColor.GetComponent<SpriteRenderer>().color = Color.blue;
-                playerColor.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.04f, 0.893f, 10f));
+                playerColor.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(0.035f, 0.900f, 10f));
                 break;
             case (2): //top right player
                 playerPicture.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player2")];
                 playerPicture.GetComponent<SpriteRenderer>().sortingOrder = 100;
-                playerPicture.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.955f, 0.900f, 10f));
+                playerPicture.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.965f, 0.860f, 10f));
                 scoreText.transform.localPosition = new Vector3(775f, 401f, 10f);
                 playerPicture.GetComponent<SpriteRenderer>().flipX = true;
                 playerColor.GetComponent<SpriteRenderer>().color = Color.red;
-                playerColor.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.955f, 0.893f, 10f));
+                playerColor.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.965f, 0.900f, 10f));
                 break;
             case (3): //bottom left player
                 playerPicture.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player3")];
-                playerPicture.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(0.04f,1 - 0.900f, 10f));
+                playerPicture.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(0.035f, 1 - 0.940f, 10f));
                 scoreText.transform.localPosition = new Vector3(-780f, -463f, 10f);
                 playerPicture.GetComponent<SpriteRenderer>().sortingOrder = 100;
                 playerColor.GetComponent<SpriteRenderer>().color = Color.green;
-                playerColor.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(0.04f, 1 - 0.908f, 10f));
+                playerColor.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(0.035f, 1 - 0.900f, 10f));
                 break;
             case (4): //bottom right player
                 playerPicture.AddComponent<SpriteRenderer>().sprite = characterList[PlayerPrefs.GetInt("Player4")];
                 playerPicture.GetComponent<SpriteRenderer>().sortingOrder = 100;
-                playerPicture.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.955f, 1 - 0.900f, 10f));
+                playerPicture.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.965f, 1 - 0.940f, 10f));
                 scoreText.transform.localPosition = new Vector3(775f, -463f, 10f);
                 playerPicture.GetComponent<SpriteRenderer>().flipX = true;
                 playerColor.GetComponent<SpriteRenderer>().color = Color.yellow;
-                playerColor.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.955f, 1 - 0.908f, 10f));
+                playerColor.transform.position = UI_Camera.ViewportToWorldPoint(new Vector3(.965f, 1 - 0.900f, 10f));
                 break;
         }
         
